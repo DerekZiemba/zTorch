9464c0d799a8ea3eda7cf5a4953c46a29f54b215
Derek Ziemba <DerekZiemba>
2014-06-19 00:12:11 -0500

    Checkpoint, deleting RootTools methods

--- root/com/stericson/RootTools/RootTools.java
+++ root/com/stericson/RootTools/RootTools.java
@@ -39,7 +39,6 @@ import com.stericson.RootTools.containers.Symlink;
 import com.stericson.RootTools.exceptions.RootDeniedException;
 import com.stericson.RootTools.execution.Command;
 import com.stericson.RootTools.execution.Shell;
-import com.stericson.RootTools.internal.Remounter;
 import com.stericson.RootTools.internal.RootToolsInternalMethods;
 import com.stericson.RootTools.internal.Runner;
 
@@ -64,6 +63,7 @@ public final class RootTools {
 	 * This implementation of RootTools has been trimed and customized to work with zTorch.  Many things have been cut out.  
 	 * Shells that are not root access have all been cut out.
 	 * BusyBox methods and applets have been cut out. 
+	 * File utilities and management have been removed as well as the ability to write and install things
 	 */
     private static RootToolsInternalMethods rim = null;
 
@@ -114,17 +114,6 @@ public final class RootTools {
     // # Public Methods #
     // ------------------
 
-    /**
-     * This will check a given binary, determine if it exists and determine that it has either the
-     * permissions 755, 775, or 777.
-     *
-     * @param util Name of the utility to check.
-     * @return boolean to indicate whether the binary is installed and has appropriate permissions.
-     */
-    public static boolean checkUtil(String util) {
-
-        return getInternals().checkUtil(util);
-    }
 
     /**
      * This will close either the root shell or the standard shell depending on what you specify.
@@ -136,32 +125,6 @@ public final class RootTools {
             Shell.closeRootShell();
     }
 
-    /**
-     * Copys a file to a destination. Because cp is not available on all android devices, we have a
-     * fallback on the cat command
-     *
-     * @param source                 example: /data/data/org.adaway/files/hosts
-     * @param destination            example: /system/etc/hosts
-     * @param remountAsRw            remounts the destination as read/write before writing to it
-     * @param preserveFileAttributes tries to copy file attributes from source to destination, if only cat is available
-     *                               only permissions are preserved
-     * @return true if it was successfully copied
-     */
-    public static boolean copyFile(String source, String destination, boolean remountAsRw,
-                                   boolean preserveFileAttributes) {
-        return getInternals().copyFile(source, destination, remountAsRw, preserveFileAttributes);
-    }
-
-    /**
-     * Deletes a file or directory
-     *
-     * @param target      example: /data/data/org.adaway/files/hosts
-     * @param remountAsRw remounts the destination as read/write before writing to it
-     * @return true if it was successfully deleted
-     */
-    public static boolean deleteFileOrDirectory(String target, boolean remountAsRw) {
-        return getInternals().deleteFileOrDirectory(target, remountAsRw);
-    }
 
     /**
      * Use this to check whether or not a file exists on the filesystem.
@@ -174,34 +137,6 @@ public final class RootTools {
         return getInternals().exists(file);
     }
 
-    /**
-     * This will try and fix a given binary. (This is for Busybox applets or Toolbox applets) By
-     * "fix", I mean it will try and symlink the binary from either toolbox or Busybox and fix the
-     * permissions if the permissions are not correct.
-     *
-     * @param util     Name of the utility to fix.
-     * @param utilPath path to the toolbox that provides ln, rm, and chmod. This can be a blank string, a
-     *                 path to a binary that will provide these, or you can use
-     *                 RootTools.getWorkingToolbox()
-     */
-    public static void fixUtil(String util, String utilPath) {
-        getInternals().fixUtil(util, utilPath);
-    }
-
-    /**
-     * This will check an array of binaries, determine if they exist and determine that it has
-     * either the permissions 755, 775, or 777. If an applet is not setup correctly it will try and
-     * fix it. (This is for Busybox applets or Toolbox applets)
-     *
-     * @param utils Name of the utility to check.
-     * @return boolean to indicate whether the operation completed. Note that this is not indicative
-     *         of whether the problem was fixed, just that the method did not encounter any
-     *         exceptions.
-     * @throws Exception if the operation cannot be completed.
-     */
-    public static boolean fixUtils(String[] utils) throws Exception {
-        return getInternals().fixUtils(utils);
-    }
 
     /**
      * @param binaryName String that represent the binary to find.
@@ -213,53 +148,6 @@ public final class RootTools {
         return getInternals().findBinary(binaryName);
     }
 
-
-
-    /**
-     * @param file String that represent the file, including the full path to the file and its name.
-     * @return An instance of the class permissions from which you can get the permissions of the
-     *         file or if the file could not be found or permissions couldn't be determined then
-     *         permissions will be null.
-     */
-    public static Permissions getFilePermissionsSymlinks(String file) {
-        return getInternals().getFilePermissionsSymlinks(file);
-    }
-
-    /**
-     * This method will return the inode number of a file. This method is dependent on having a version of
-     * ls that supports the -i parameter.
-     *
-     * @param file path to the file that you wish to return the inode number
-     * @return String The inode number for this file or "" if the inode number could not be found.
-     */
-    public static String getInode(String file) {
-        return getInternals().getInode(file);
-    }
-
-    /**
-     * This will return an ArrayList of the class Mount. The class mount contains the following
-     * property's: device mountPoint type flags
-     * <p/>
-     * These will provide you with any information you need to work with the mount points.
-     *
-     * @return <code>ArrayList<Mount></code> an ArrayList of the class Mount.
-     * @throws Exception if we cannot return the mount points.
-     */
-    public static ArrayList<Mount> getMounts() throws Exception {
-        return getInternals().getMounts();
-    }
-
-    /**
-     * This will tell you how the specified mount is mounted. rw, ro, etc...
-     * <p/>
-     *
-     * @param path The mount you want to check
-     * @return <code>String</code> What the mount is mounted as.
-     * @throws Exception if we cannot determine how the mount is mounted.
-     */
-    public static String getMountedAs(String path) throws Exception {
-        return getInternals().getMountedAs(path);
-    }
 
     /**
      * This will return the environment variable PATH
@@ -312,109 +200,6 @@ public final class RootTools {
         return RootTools.getShell(root, 25000);
     }
 
-    /**
-     * Get the space for a desired partition.
-     *
-     * @param path The partition to find the space for.
-     * @return the amount if space found within the desired partition. If the space was not found
-     *         then the value is -1
-     * @throws TimeoutException
-     */
-    public static long getSpace(String path) {
-        return getInternals().getSpace(path);
-    }
-
-    /**
-     * This will return a String that represent the symlink for a specified file.
-     * <p/>
-     *
-     * @param file path to the file to get the Symlink for. (must have absolute path)
-     * @return <code>String</code> a String that represent the symlink for a specified file or an
-     *         empty string if no symlink exists.
-     */
-    public static String getSymlink(String file) {
-        return getInternals().getSymlink(file);
-    }
-
-    /**
-     * This will return an ArrayList of the class Symlink. The class Symlink contains the following
-     * property's: path SymplinkPath
-     * <p/>
-     * These will provide you with any Symlinks in the given path.
-     *
-     * @param path path to search for Symlinks.
-     * @return <code>ArrayList<Symlink></code> an ArrayList of the class Symlink.
-     * @throws Exception if we cannot return the Symlinks.
-     */
-    public static ArrayList<Symlink> getSymlinks(String path) throws Exception {
-        return getInternals().getSymlinks(path);
-    }
-
-    /**
-     * This will return to you a string to be used in your shell commands which will represent the
-     * valid working toolbox with correct permissions. For instance, if Busybox is available it will
-     * return "busybox", if busybox is not available but toolbox is then it will return "toolbox"
-     *
-     * @return String that indicates the available toolbox to use for accessing applets.
-     */
-    public static String getWorkingToolbox() {
-        return getInternals().getWorkingToolbox();
-    }
-
-    /**
-     * Checks if there is enough Space on SDCard
-     *
-     * @param updateSize size to Check (long)
-     * @return <code>true</code> if the Update will fit on SDCard, <code>false</code> if not enough
-     *         space on SDCard. Will also return <code>false</code>, if the SDCard is not mounted as
-     *         read/write
-     */
-    public static boolean hasEnoughSpaceOnSdCard(long updateSize) {
-        return getInternals().hasEnoughSpaceOnSdCard(updateSize);
-    }
-
-    /**
-     * Checks whether the toolbox or busybox binary contains a specific util
-     *
-     * @param util
-     * @param box  Should contain "toolbox" or "busybox"
-     * @return true if it contains this util
-     */
-    public static boolean hasUtil(final String util, final String box) {
-        //TODO Convert this to use the new shell.
-        return getInternals().hasUtil(util, box);
-    }
-
-    /**
-     * This method can be used to unpack a binary from the raw resources folder and store it in
-     * /data/data/app.package/files/ This is typically useful if you provide your own C- or
-     * C++-based binary. This binary can then be executed using sendShell() and its full path.
-     *
-     * @param context  the current activity's <code>Context</code>
-     * @param sourceId resource id; typically <code>R.raw.id</code>
-     * @param destName destination file name; appended to /data/data/app.package/files/
-     * @param mode     chmod value for this file
-     * @return a <code>boolean</code> which indicates whether or not we were able to create the new
-     *         file.
-     */
-    public static boolean installBinary(Context context, int sourceId, String destName, String mode) {
-        return getInternals().installBinary(context, sourceId, destName, mode);
-    }
-
-    /**
-     * This method can be used to unpack a binary from the raw resources folder and store it in
-     * /data/data/app.package/files/ This is typically useful if you provide your own C- or
-     * C++-based binary. This binary can then be executed using sendShell() and its full path.
-     *
-     * @param context    the current activity's <code>Context</code>
-     * @param sourceId   resource id; typically <code>R.raw.id</code>
-     * @param binaryName destination file name; appended to /data/data/app.package/files/
-     * @return a <code>boolean</code> which indicates whether or not we were able to create the new
-     *         file.
-     */
-    public static boolean installBinary(Context context, int sourceId, String binaryName) {
-        return installBinary(context, sourceId, binaryName, "700");
-    }
 
     /**
      * This method checks whether a binary is installed.
@@ -428,8 +213,6 @@ public final class RootTools {
         return getInternals().isBinaryAvailable(context, binaryName);
     }
 
-
-
     /**
      * @return <code>true</code> if your app has been given root access.
      * @throws TimeoutException if this operation times out. (cannot determine if access is given)
@@ -438,12 +221,6 @@ public final class RootTools {
         return getInternals().isAccessGiven();
     }
 
-    /**
-     * @return <code>true</code> if BusyBox was found.
-     */
-    public static boolean isBusyboxAvailable() {
-        return findBinary("busybox");
-    }
 
     public static boolean isNativeToolsReady(int nativeToolsId, Context context) {
         return getInternals().isNativeToolsReady(nativeToolsId, context);
@@ -479,26 +256,7 @@ public final class RootTools {
         return getInternals().killProcess(processName);
     }
 
-    /**
-     * This will launch the Android market looking for BusyBox
-     *
-     * @param activity pass in your Activity
-     */
-    public static void offerBusyBox(Activity activity) {
-        getInternals().offerBusyBox(activity);
-    }
 
-    /**
-     * This will launch the Android market looking for BusyBox, but will return the intent fired and
-     * starts the activity with startActivityForResult
-     *
-     * @param activity    pass in your Activity
-     * @param requestCode pass in the request code
-     * @return intent fired
-     */
-    public static Intent offerBusyBox(Activity activity, int requestCode) {
-        return getInternals().offerBusyBox(activity, requestCode);
-    }
 
     /**
      * This will launch the Android market looking for SuperUser
@@ -521,26 +279,6 @@ public final class RootTools {
         return getInternals().offerSuperUser(activity, requestCode);
     }
 
-    /**
-     * This will take a path, which can contain the file name as well, and attempt to remount the
-     * underlying partition.
-     * <p/>
-     * For example, passing in the following string:
-     * "/system/bin/some/directory/that/really/would/never/exist" will result in /system ultimately
-     * being remounted. However, keep in mind that the longer the path you supply, the more work
-     * this has to do, and the slower it will run.
-     *
-     * @param file      file path
-     * @param mountType mount type: pass in RO (Read only) or RW (Read Write)
-     * @return a <code>boolean</code> which indicates whether or not the partition has been
-     *         remounted as specified.
-     */
-    public static boolean remount(String file, String mountType) {
-        // Recieved a request, get an instance of Remounter
-        Remounter remounter = new Remounter();
-        // send the request.
-        return (remounter.remount(file, mountType));
-    }
 
     /**
      * This restarts only Android OS without rebooting the whole device. This does NOT work on all
